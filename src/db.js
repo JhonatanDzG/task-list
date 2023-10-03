72dac8cc74ce22465331ff0a2250c29df561c73f
JhonatanDzG <jhonatandzg@gmail.com>
2023-10-03 14:09:50 -0500

    feat: hardcodelete

--- src/db.js
+++ src/db.js
@@ -2,10 +2,10 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
+  password: "JhonatanDzG117",
   host: "localhost",
   port: 5432,
   database: "tasksdb",
 });
-console.log(pool);
 
 module.exports = pool;
